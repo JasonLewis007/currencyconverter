e571b5b2685f7d0110328887ca048e43032d92dd
Jason Lewis <jlewis@harperlove.com>
2023-12-08 12:03:42 -0500

    5th commit

--- currency-converter/src/components/CurrencyConverter.js
+++ currency-converter/src/components/CurrencyConverter.js
@@ -1,5 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import axious from 'axios';
+// src/components/CurrencyConverter.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const headerStyle = {
+  backgroundColor: 'lightgoldenrodyellow',
+  height: '10vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  transition: 'background-color 0.5s ease',
+  fontFamily: 'IBM Plex Mono, monospace', // Apply the Google Font
+};
+
+const containerStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '20px',
+};
+
+const labelStyle = {
+  fontFamily: 'IBM Plex Mono, monospace', // Apply the Google Font
+  marginBottom: '10px',
+};
+
+const buttonStyle = {
+  fontFamily: 'IBM Plex Mono, monospace', // Apply the Google Font
+  marginBottom: '10px',
+  cursor: 'pointer',
+};
 
 function CurrencyConverter() {
   const [amount, setAmount] = useState('');
@@ -8,19 +37,19 @@ function CurrencyConverter() {
   const [currencies, setCurrencies] = useState([]);
   const [result, setResult] = useState('');
 
-    useEffect(() => {
-         // Fetch available currencies when the component mounts
+  useEffect(() => {
+    // Fetch available currencies when the component mounts
     async function fetchCurrencies() {
-        try {
-          const response = await axios.get('YOUR_API_ENDPOINT');
-          const availableCurrencies = Object.keys(response.data.rates);
-          setCurrencies(availableCurrencies);
-        } catch (error) {
-          console.error(error);
-        }
+      try {
+        const response = await axios.get('YOUR_API_ENDPOINT');
+        const availableCurrencies = Object.keys(response.data.rates);
+        setCurrencies(availableCurrencies);
+      } catch (error) {
+        console.error(error);
       }
+    }
 
-      fetchCurrencies();
+    fetchCurrencies();
   }, []);
 
   const convert = async () => {
@@ -39,48 +68,65 @@ function CurrencyConverter() {
 
   return (
     <div>
-      <h1>Currency Converter</h1>
+      <header style={headerStyle}>
+        <h1 style={{ margin: 0, color: 'darkgoldenrod' }}>Jason's Currency Converter</h1>
+      </header>
 
-      <label htmlFor="amount">Amount:</label>
-      <input
-        type="number"
-        id="amount"
-        placeholder="Enter amount"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
+      <div style={containerStyle}>
+        <label htmlFor="amount" style={labelStyle}>
+          Amount:
+        </label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Enter amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          style={{ marginBottom: '10px' }}
+        />
 
-      <label htmlFor="fromCurrency">From Currency:</label>
-      <select
-        id="fromCurrency"
-        value={fromCurrency}
-        onChange={(e) => setFromCurrency(e.target.value)}
-      >
-        {currencies.map((currency) => (
-          <option key={currency} value={currency}>
-            {currency}
-          </option>
-        ))}
-      </select>
+        <label htmlFor="fromCurrency" style={labelStyle}>
+          From Currency:
+        </label>
+        <select
+          id="fromCurrency"
+          value={fromCurrency}
+          onChange={(e) => setFromCurrency(e.target.value)}
+          style={{ marginBottom: '10px' }}
+        >
+          {currencies.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
 
-      <label htmlFor="toCurrency">To Currency:</label>
-      <select
-        id="toCurrency"
-        value={toCurrency}
-        onChange={(e) => setToCurrency(e.target.value)}
-      >
-        {currencies.map((currency) => (
-          <option key={currency} value={currency}>
-            {currency}
-          </option>
-        ))}
-      </select>
+        <label htmlFor="toCurrency" style={labelStyle}>
+          To Currency:
+        </label>
+        <select
+          id="toCurrency"
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+          style={{ marginBottom: '10px' }}
+        >
+          {currencies.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
 
-      <button onClick={convert}>Convert</button>
+        <button onClick={convert} style={buttonStyle}>
+          Convert
+        </button>
 
-      <p id="result">Result: {result}</p>
+        <p id="result">Result: {result}</p>
+      </div>
     </div>
   );
 }
 
 export default CurrencyConverter;
+
+
